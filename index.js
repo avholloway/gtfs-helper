bc6bffed652787fab9913dd857c24350f2de6f65
Anthony Holloway <avholloway@gmail.com>
2026-02-12 17:21:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (index, res) => {
+app.get('/', async (index, res) => {
 
   try {
     const response = await fetch("https://gtfsapi.translink.ca/v3/gtfsposition?apikey=EuftOZI6oDloPgi25jdw");
