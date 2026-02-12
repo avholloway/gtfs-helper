3e465acc3c4c67af2ff40c1174aaa0e3294fc1c0
Anthony Holloway <avholloway@gmail.com>
2026-02-12 17:17:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 import GtfsRealtimeBindings from "gtfs-realtime-bindings";
+import fetch from "node-fetch";
 import express from 'express';
 const app = express();
 
-// Essential: This allows your app to parse incoming JSON data
 app.use(express.json());
 
-// 1 & 3. GET endpoint to check if it's live and return JSON
 app.get('/', (index, res) => {
-  res.json({ message: "Welcome to gtfs-helper.render.com!" });
-});
 
-// 2 & 3. POST endpoint to receive data and return JSON
-app.post('/data', (req, res) => {
-  const receivedData = req.body;
-  console.log("Data received");
-
-  const buffer = await response.arrayBuffer();
-  const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
-  let entities = [];
-  feed.entity.forEach((entity) => {
+  try {
+    const response = await fetch("https://gtfsapi.translink.ca/v3/gtfsposition?apikey=EuftOZI6oDloPgi25jdw");
+    if (!response.ok) {
+      const error = new Error(`${response.url}: ${response.status} ${response.statusText}`);
+      error.response = response;
+      throw error;
+    }
+    const buffer = await response.arrayBuffer();
+    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(buffer));
+    let entities = [];
+    feed.entity.forEach((entity) => {
       entities.push(entity.toJSON());
-  });
+    });
+    res.json(entities);
+  } catch (error) {
+    res.json({"status":"error","error":error});
+  }
   
-  res.status(200).json({
-    status: "success",
-    data: entities
-  });
 });
 
-// Important: Render assigns a port dynamically via process.env.PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
